a72f1653320e64aaa3d432e35c819b7ae034df41
Admin <caophuoc6@gmail.com>
2023-07-28 10:00:56 +0700

    Init source code

---
+++ EventBusTraning/EventBusTraning/EventBus/EventBus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBusTraning.EventBus
+{
+    public class EventBus
+    {
+        private readonly Dictionary<Type, List<Delegate>> eventHandlers = new Dictionary<Type, List<Delegate>>();
+        public void Subscribe<T>(Action<T> eventHandler)
+        {
+            Type eventType = typeof(T);
+            if (!eventHandlers.ContainsKey(eventType))
+            {
+                eventHandlers[eventType] = new List<Delegate>();
+            }
+
+            eventHandlers[eventType].Add(eventHandler);
+        }
+        public void Unsubscribe<T>(Action<T> eventHandler)
+        {
+            Type eventType = typeof(T);
+            if (eventHandlers.ContainsKey(eventType))
+            {
+                eventHandlers[eventType].Remove(eventHandler);
+            }
+        }
+        public void Publish<T>(T @event)
+        {
+            Type eventType = typeof(T);
+            if (eventHandlers.ContainsKey(eventType))
+            {
+                foreach (var handler in eventHandlers[eventType])
+                {
+                    ((Action<T>)handler)(@event);
+                }
+            }
+        }
+    }
+}
---
+++ EventBusTraning/EventBusTraning/Program.cs
@@ -0,0 +1,28 @@
+﻿using EventBusTraning.EventBus;
+using EventBusTraning.Method;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EventBus eventBus = new EventBus();
+
+        // Đăng ký xử lý sự kiện UserRegisteredEvent
+        eventBus.Subscribe<UserRegisteredEvent>(OnUserRegistered);
+
+        // Phát sinh sự kiện UserRegisteredEvent
+        UserRegisteredEvent userRegisteredEvent = new UserRegisteredEvent { UserName = "JohnDoe" };
+        eventBus.Publish(userRegisteredEvent);
+    }
+
+    static void OnUserRegistered(UserRegisteredEvent userRegisteredEvent)
+    {
+        Console.WriteLine($"User {userRegisteredEvent.UserName} has been registered.");
+    }
+}
+//Khi chạy chương trình, bạn sẽ nhận được thông báo "User JohnDoe has been registered." do sự kiện UserRegisteredEvent đã được phát sinh và xử lý bởi phương thức OnUserRegistered.
+
+
+
+
+
